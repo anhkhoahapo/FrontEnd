9958ec83bbf682f2538721810082212f48924a0c
anhkhoa45 <khoa.1997a@gmail.com>
2017-10-17 10:20:38 +0700

    Big update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,27 @@ window.onload = function() {
 
     window.addEventListener('scroll', scrollHandle);
 
+    function scrollHandle(e) {
+        let scrollY = window.scrollY;
+        let navbar = document.getElementById("main-nav");
+        let btnScrTop = document.getElementById("btn-scrolltop");
+        let btnNextSection = document.getElementById("btn-nextsection");
+
+        navbar.style.transition = 'all 0.5s';
+
+        if (scrollY > 100) {
+            navbar.style.backgroundColor = 'white';
+            navbar.style.boxShadow = '0 0 20px 0 grey';
+            btnScrTop.style.display = 'block';
+            btnNextSection.style.display = 'none';
+        } else {
+            navbar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+            navbar.style.boxShadow = 'none';
+            btnScrTop.style.display = 'none';
+            btnNextSection.style.display = 'block';
+        }
+    }
+
     let prevSlide = document.getElementById("btn-prev");
     let nextSlide = document.getElementById("btn-next");
     let slideItems = document.getElementsByClassName("slide-item");
@@ -11,52 +32,30 @@ window.onload = function() {
 
     toSlide(curSlide);
 
-    prevSlide.addEventListener('click', function() {
+    prevSlide.addEventListener('click', () => {
         toSlide((curSlide - 1 + slideItems.length) % slideItems.length);
     });
-    nextSlide.addEventListener('click', function() {
+    nextSlide.addEventListener('click', () => {
         toSlide((curSlide + 1) % slideItems.length);
     });
 
     for (let i = 0; i < dots.length; i++) {
         dots[i].id = i;
-        dots[i].addEventListener('click', function() {
+        dots[i].addEventListener('click', () => {
             toSlide(this.id);
         });
     }
 
     function toSlide(n) {
         curSlide = n;
-        slideItems[curSlide].classList.remove('sr-only');
+        slideItems[curSlide].style.display = 'block';
         dots[curSlide].style.backgroundColor = '#fff';
 
         for (let i = 0; i < slideItems.length; i++) {
             if (i != curSlide) {
-                slideItems[i].classList.add('sr-only');
+                slideItems[i].style.display = 'none';
                 dots[i].style.backgroundColor = 'rgba(0,0,0,0)';
             }
         }
     }
-
-    function scrollHandle(e) {
-        let scrollY = window.scrollY;
-        let navbar = document.getElementById('main-nav');
-        let btnScrTop = document.getElementById('btn-scrolltop');
-        let btnNextSection = document.getElementById('btn-nextsection');
-
-        navbar.style.transition = "all 0.5s";
-
-        if (scrollY > 100) {
-            navbar.style.backgroundColor = 'white';
-            navbar.style.boxShadow = '0 0 20px 0 grey';
-            btnScrTop.classList.remove('sr-only');
-            btnNextSection.classList.add('sr-only');
-        } else {
-            navbar.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-            navbar.style.boxShadow = 'none';
-            btnScrTop.classList.add('sr-only');
-            btnNextSection.classList.remove('sr-only');
-        }
-    }
-
 }
